1860d8dfcc7bf1edc9b1db828c75011199156610
Cobb304 <spqrmahan@gmail.com>
2026-05-29 23:55:28 +0330

    05: Search Filters

---
+++ src/app/(app)/(home)/search-filters/Categories.tsx
@@ -0,0 +1,25 @@
+import { Category } from "@/payload-types";
+
+import CategoryDropdown from "./CategoryDropdown";
+
+interface Props {
+  data: any;
+}
+
+export default function Categories({ data }: Props) {
+  return (
+    <div className="relative w-full">
+      <div className="flex flex-nowrap items-center">
+        {data.map((category: Category) => (
+          <div key={category.id}>
+            <CategoryDropdown
+              category={category}
+              isActive={false}
+              isNavigaitonHovered={false}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
